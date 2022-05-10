4784565ee97de3429f349b97ecef61f0d648ad8d
kajela <titoyut56@gmail.com>
2022-05-10 06:56:46 -0400

    Initial commit

---
+++ src/object.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor;
+
+public class object {
+    int y = 2;
+    String name = "joy";
+    public static void main(String[] args) {
+        object myObj1 = new object();
+        object myObj2 = new object();
+        System.out.println(myObj1.y);
+        System.out.println(myObj2.name);
+    }
+}
